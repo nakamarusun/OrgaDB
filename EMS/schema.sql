896ddb11f791051e0e412834942efe0e97d0717e
n1c0201 <56902710+n1c0201@users.noreply.github.com>
2020-12-03 01:15:37 +0700

    Updated schema.sql

--- EMS/schema.sql
+++ EMS/schema.sql
@@ -1,30 +1,32 @@
-CREATE TABLE Event(
+CREATE TABLE Events(
     id INT(255) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     Event_name VARCHAR(40) NOT NULL,
     Venue VARCHAR(40) NOT NULL,
-    BUDGET INT(255), NOT NULL
-);
-
-CREATE TABLE Clearance(
-    Member_id INT(30) NOT NULL,
-    Clearance_level INT(10) NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Member_id) REFERENCES Members(id),
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
+    Budget INT(255) NOT NULL
 );
 
 CREATE TABLE Members(
     Full_Name VARCHAR(50) NOT NULL,
     Position VARCHAR(50) NOT NULL,
-    id INT(30) PRIMARY KEY NOT NULL
+    Id INT(30) PRIMARY KEY NOT NULL
 );
+
+CREATE TABLE Clearance(
+    Member_id INT(30) NOT NULL,
+    Clearance_level ENUM("1", "2", "3", "4", "5") NOT NULL,
+    Event_id INT(255) NOT NULL,
+    FOREIGN KEY (Member_id) REFERENCES Members(id),
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
+);
+
+
 
 CREATE TABLE Event_Committee(
     Event_id INT(255) NOT NULL,
     Member_id INT(255) NOT NULL,
     Member_Role VARCHAR(50) NOT NULL,
     FOREIGN KEY (Member_id) REFERENCES Members(id),
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
 );
 
 CREATE TABLE Feedback(
@@ -32,7 +34,7 @@ CREATE TABLE Feedback(
     Rating INT(10) NOT NULL,
     Comments VARCHAR(255) NOT NULL,
     Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
 );
 
 CREATE TABLE Guests(
@@ -41,30 +43,30 @@ CREATE TABLE Guests(
     Phone_Number INT(25) NOT NULL,
     Category VARCHAR(50) NOT NULL,
     Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
 );
 
 CREATE TABLE Expenses(
     Expense_Type VARCHAR(50) NOT NULL,
     Amount INT(255) NOT NULL,
     Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
 );
 
 CREATE TABLE Sponsor(
     Full_Name VARCHAR(50) NOT NULL,
     Sponsor_Type VARCHAR(10) NOT NULL,
-    id INT(50) PRIMARY KEY NOT NULL,
+    Id INT(50) PRIMARY KEY NOT NULL,
     Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id)
-)
+    FOREIGN KEY (Event_id) REFERENCES Events(id)
+);
 
 CREATE TABLE Inventory(
     Item_Name VARCHAR(255) NOT NULL,
     Item_Amount INT(50) NOT NULL,
     Sponsor_id INT(50),
     Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id),
+    FOREIGN KEY (Event_id) REFERENCES Events(id),
     FOREIGN KEY (Sponsor_id) REFERENCES Sponsor(id)
 );
 
@@ -73,6 +75,6 @@ CREATE TABLE Income(
     Amount INT(100) NOT NULL,
     Sponsor_id INT(50),
     Event_id INT(50) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Event(id),
+    FOREIGN KEY (Event_id) REFERENCES Events(id),
     FOREIGN KEY (Sponsor_id) REFERENCES Sponsor(id)
 );
