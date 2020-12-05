fa262a37cd8111323f52549476df8dc6718e5f0f
Aric <hernandoaric@gmail.com>
2020-12-05 19:08:42 +0700

    changed onboarding designs

--- EMS/controllers/user.py
+++ EMS/controllers/user.py
@@ -1,13 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 bp = Blueprint("user", __name__, url_prefix="/user")
 
 @bp.route("/login")
 def login():
     # TODO @bently: Create login
-    return "login"
+    login_text = True
+    return render_template('login.html',login_text =login_text)
 
 @bp.route("/register")
 def register():
     # TODO @bently: Create register
-    return "register"
+    login_text = False
+    return render_template('register.html', login_text=login_text)
