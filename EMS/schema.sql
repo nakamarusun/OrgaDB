b582b3cee689c122465d10222221694c6544ac4c
n1c0201 <56902710+n1c0201@users.noreply.github.com>
2020-12-08 20:46:49 +0700

    fixed capitalisation errors in schema.sql

--- EMS/schema.sql
+++ EMS/schema.sql
@@ -12,8 +12,8 @@ DROP TABLE IF EXISTS Login_cred;
 
 
 CREATE TABLE Events(
-    id INT(255) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    Event_name VARCHAR(40) NOT NULL,
+    Id INT(255) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    Event_Name VARCHAR(40) NOT NULL,
     Venue VARCHAR(40) NOT NULL,
     Budget INT(255) NOT NULL
 );
@@ -25,28 +25,28 @@ CREATE TABLE Members(
 );
 
 CREATE TABLE Clearance(
-    Member_id INT(30) NOT NULL,
-    Clearance_level ENUM("1", "2", "3", "4", "5") NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Member_id) REFERENCES Members(id),
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    Member_Id INT(30) NOT NULL,
+    Clearance_Level ENUM("1", "2", "3", "4", "5") NOT NULL,
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Member_id) REFERENCES Members(Id),
+    FOREIGN KEY (Event_id) REFERENCES Events(Id)
 );
 
 
 
 CREATE TABLE Event_Committee(
-    Event_id INT(255) NOT NULL,
-    Member_id INT(255) NOT NULL,
+    Event_Id INT(255) NOT NULL,
+    Member_Id INT(255) NOT NULL,
     Member_Role VARCHAR(50) NOT NULL,
-    FOREIGN KEY (Member_id) REFERENCES Members(id),
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    FOREIGN KEY (Member_Id) REFERENCES Members(Id),
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id)
 );
 
 CREATE TABLE Feedback(
     Rating INT(10) NOT NULL,
     Comments VARCHAR(255) NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id)
 );
 
 CREATE TABLE Guests(
@@ -54,16 +54,16 @@ CREATE TABLE Guests(
     Email VARCHAR(50) NOT NULL,
     Phone_Number INT(25) NOT NULL,
     Category VARCHAR(50) NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id)
 );
 
 CREATE TABLE Expenses(
     Expense_Type ENUM("Venue", "Marketing", "Food and Beverage", "Art Supplies", "Equipment", "Other") NOT NULL,
-    Other_expense VARCHAR(50),
+    Other_Expense VARCHAR(50),
     Amount INT(255) NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id)
 );
 
 CREATE TABLE Sponsor(
@@ -72,32 +72,32 @@ CREATE TABLE Sponsor(
     Sponsor_Address VARCHAR(50) NOT NULL,
     Phone_Number VARCHAR(50) NOT NULL,
     Sponsor_Type VARCHAR(10) NOT NULL,
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id)
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id)
 );
 
 CREATE TABLE Inventory(
-    Inventory_id INT(50) PRIMARY KEY NOT NULL,
+    Inventory_Id INT(50) PRIMARY KEY NOT NULL,
     Item_Name VARCHAR(255) NOT NULL,
     Item_Quantity INT(50) NOT NULL,
-    Sponsor_id INT(50),
-    Event_id INT(255) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id),
-    FOREIGN KEY (Sponsor_id) REFERENCES Sponsor(id)
+    Sponsor_Id INT(50),
+    Event_Id INT(255) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id),
+    FOREIGN KEY (Sponsor_Id) REFERENCES Sponsor(Id)
 );
 
 CREATE TABLE Income(
     Income_Type ENUM("Sponsor", "Ticket Sales", "Merchandise", "Other") NOT NULL,
-    Other_income VARCHAR(50),
+    Other_Income VARCHAR(50),
     Amount INT(100) NOT NULL,
-    Sponsor_id INT(50),
-    Event_id INT(50) NOT NULL,
-    FOREIGN KEY (Event_id) REFERENCES Events(id),
-    FOREIGN KEY (Sponsor_id) REFERENCES Sponsor(id)
+    Sponsor_Id INT(50),
+    Event_Id INT(50) NOT NULL,
+    FOREIGN KEY (Event_Id) REFERENCES Events(Id),
+    FOREIGN KEY (Sponsor_Id) REFERENCES Sponsor(Id)
 );
 
 CREATE TABLE Login_cred(
     Id VARCHAR(50) NOT NULL,
-    pass VARCHAR(50) NOT NULL,
+    Pass VARCHAR(50) NOT NULL,
     Email VARCHAR(50) NOT NULL
 );
