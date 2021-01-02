d3b20f98cb18a5e467188c7612fafcaa1f3385f0
Jason Christian H <nakamarujc@gmail.com>
2021-01-03 00:18:52 +0700

    Removed git merge conflict marker

--- EMS/controllers/index.py
+++ EMS/controllers/index.py
@@ -5,8 +5,4 @@ bp = Blueprint("index", __name__, url_prefix="/")
 @bp.route("/")
 def index():
     # TODO @aric: Create index.html here and use render_template
-<<<<<<< HEAD
     return render_template("index.html", debug = True)
-=======
-    return render_template("finance.html", debug = True)
->>>>>>> html_testing_ground
