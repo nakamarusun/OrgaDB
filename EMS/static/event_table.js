858f82e7b8ffe36101617f575695bec4a6c71b68
Aric <hernandoaric@gmail.com>
2021-01-02 22:37:22 +0700

    added add record functionality

--- EMS/static/event_table.js
+++ EMS/static/event_table.js
@@ -2,16 +2,57 @@ $(document).ready(function(){
     // Tabs and table handler
     $('.content:first').show();
     $('.tabs:first').addClass('active');
+    
+    // Formats number to currency
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'IDR',
+    });
+
+    // Keeps track of the active table, appends posted stuff into the table.
+    var activeTable = 0;
+
     $('.tabs').click(function(event){
-        index = $(this).index();
+        activeTable = $(this).index();
+
         $('.tabs').removeClass('active');
         $(this).addClass('active');
-        $('.content').hide()
-        $('.content').eq(index).show()
-    })
+        $('.content').hide();
+        $('.content').eq(activeTable).show();
+    });
+
+    $('form').on("submit",function(e){
+        // Serializes form data into string
+        var serializedForm = $(this).serialize();
+        
+        var serializedArray = $(this).serializeArray();
+
+        var currentPath = window.location.pathname;
+        $.ajax({
+            type : "POST",
+            url : currentPath + "add",
+            data : serializedForm,
+            success : function(){
+                var row = '';
+                for(x in serializedArray){
+                    if(x == 2){
+                        row += '<td class="border-2 h-10">' +  formatter.format(serializedArray[x]['value']) + '</td>';
+                    } else{
+                        row += '<td class="border-2 h-10">' +  (serializedArray[x]['value']) + '</td>';
+                    }
+                }
+                console.log(row)
+                $('.content tbody').eq(activeTable).append('<tr>' + row + '</tr>');
+                alert("Succesfully added record");
+            }
+        });
+        
+        e.preventDefault();
+    });
+    
 
     var rotation = 0;
-    jQuery.fn.rotate = function(degrees) {
+    jQuery.fn.rotate180 = function(degrees) {
         $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
         return $(this);
     };
@@ -19,6 +60,6 @@ $(document).ready(function(){
     $('.show-form').click(function() {
         rotation += 180;
         $('.form-panel').slideToggle();
-        $('.chevron-down').rotate(rotation);
+        $('.chevron-down').rotate180(rotation);
     });
-})
+});
