dfc69f47266d38e784c3cb49e424225438b5c102
Aric <hernandoaric@gmail.com>
2021-01-06 21:29:43 +0700

    implemented input validation to edit

--- EMS/static/input_validator.js
+++ EMS/static/input_validator.js
@@ -1,18 +1,43 @@
-export function validatePhone(string){
+function validatePhone(string){
     const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    if(string === ""){
+        return false
+    }
     return (re.test(string))
 }
 
-export function validateEmail(email) {
+function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email === ""){
+        return false
+    }
     return re.test(String(email).toLowerCase());
 }
 
-export function validateName(name){
+function validateName(name){
     const re = /^[A-Za-z\ ]+$/
+    if(name === ""){
+        return false
+    }
     return re.test(name)
 }
-export function validateInt(num){
+function validateInt(num){
     const re = /^[0-9]+$/
+    if(num === ""){
+        return false
+    }
     return re.test(num)
+}
+function validateMoney(num){
+    const re = /^[0-9/.]+$/
+    if(num === ""){
+        return false
+    }
+    return re.test(num)
+}
+function validateEmpty(string){
+    if(string === ""){
+        return false
+    }
+    return true
 }
