0e0921d6a45b0efb49a6627c6dc43c1ffcf6ed56
Aric <hernandoaric@gmail.com>
2021-01-06 20:40:12 +0700

    added input validators

---
+++ EMS/static/input_validator.js
@@ -0,0 +1,18 @@
+export function validatePhone(string){
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    return (re.test(string))
+}
+
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export function validateName(name){
+    const re = /^[A-Za-z\ ]+$/
+    return re.test(name)
+}
+export function validateInt(num){
+    const re = /^[0-9]+$/
+    return re.test(num)
+}
