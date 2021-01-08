778ab496a2f2e59db317770485f21decd2210842
Jason Christian H <nakamarujc@gmail.com>
2021-01-08 21:22:08 +0700

    The index page now shows all the events that the user have clearance to view

--- EMS/controllers/index.py
+++ EMS/controllers/index.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for
+import EMS.db as db
 
 bp = Blueprint("index", __name__, url_prefix="/")
 
@@ -12,7 +13,24 @@ def prompt_login():
 
 @bp.route("/")
 def index():
-    return render_template("index.html")
+    # Get all the events taht is available for the current user.
+    cursor = db.get_db().cursor()
+    cursor.execute("SELECT e.Id, Event_Name, Clearance_Level FROM Clearance c JOIN Events e ON c.Event_Id=e.Id WHERE c.Member_Id=%s;", (
+        session["member_id"],
+    ))
+
+    events = []
+    # Gets all the events available for the user
+    for ev in cursor.fetchall():
+        # If the clearance is not 1, then show the event
+        if ev[2] != 1:
+            events.append({
+                "username": "Bro",
+                "event_id": ev[0],
+                "event_name": ev[1]
+            })
+
+    return render_template("index.html", event_dict=events)
 
 
 @bp.route("/admin")
